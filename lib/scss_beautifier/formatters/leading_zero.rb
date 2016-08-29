b64fc2bd6cf2833e5842d77952cba3413cdd078f
Ivan Tse <ivan.tse1@gmail.com>
2016-08-29 18:09:18 -0400

    Fix LeadingZero

--- lib/scss_beautifier/formatters/leading_zero.rb
+++ lib/scss_beautifier/formatters/leading_zero.rb
@@ -3,8 +3,7 @@ class SCSSBeautifier::Formatters::LeadingZero < Sass::Tree::Visitors::Base
   def visit_prop(node)
     if Sass::Script::Tree::Literal === node.value && Sass::Script::Value::String === node.value.value
       node_value = node.value.value
-
-      if node.value.value.to_s.match(/\b0./)
+      if node.value.value.to_s.match(/\b0\./)
         node.instance_variable_set(:@value, Sass::Script::Value::String.new(node.value.value.to_s.gsub(/\b0/, '')))
       end
     end
