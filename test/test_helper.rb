4f99821223da313217e4a8f5ae909cd2a1022405
Joe Natalzia <jnatalzia@paperlesspost.com>
2016-08-31 10:15:57 -0400

    Add tests for all formatters
    TODO: Update error messaging on failure to better point to trouble area

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,11 @@ require 'scss_beautifier'
 require 'minitest/autorun'
 
 def test_style_rule(formatter, test_file)
+  puts "Testing #{formatter}"
   old_contents = File.readlines("test/example_scss/#{test_file}.scss").join
-  expected_contents = File.readlines("test/example_scss/beautified/#{test_file}.scss").join
+  expected_contents = File.readlines("test/example_scss/beautified/#{test_file}.scss").join.strip
   engine = Sass::Engine.new(old_contents, cache: false, syntax: :scss)
   tree = engine.to_tree
   formatter.visit tree
-  return expected_contents == tree.to_scss
+  return expected_contents == tree.to_scss.strip
 end
