75aab319e373564968dc0c65b13282b680326df8
Joe Natalzia <jnatalzia@paperlesspost.com>
2016-09-02 13:01:43 -0400

    Fix bug with color hex expander
    Previously it didn't take into account the possibility of more
    than just a hex in the string. This change updates the regex
    to handle prop values with more than just the hex

--- lib/scss_beautifier/formatters/color.rb
+++ lib/scss_beautifier/formatters/color.rb
@@ -13,10 +13,12 @@ class SCSSBeautifier::Formatters::Color < SCSSBeautifier::Formatters::Base
         color_value = Sass::Script::Value::Color.new(color)
         color_value.options = {}
         item.instance_variable_set(:@value, Sass::Script::Value::String.new(color_value.inspect))
-      elsif item.value.value =~ /(#\h{3})(?!\h)/
-        hex = item.value.value
+      elsif matches = item.value.value.match(/([\w\W]*)(#\h{3})(?!\h)([\w\W]*)/)
+        str_start = matches[1]
+        str_end = matches[3]
+        hex = matches[2]
         long_form = [hex[0..1], hex[1], hex[2], hex[2], hex[3], hex[3]].join
-        item.instance_variable_set(:@value, Sass::Script::Value::String.new(long_form))
+        item.instance_variable_set(:@value, Sass::Script::Value::String.new(str_start + long_form + str_end))
       end
     end
   end
