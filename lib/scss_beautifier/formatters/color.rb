d1931f8a2dbd9be37d2bb019d90eca21c03b3f4b
Joe Natalzia <jnatalzia@paperlesspost.com>
2016-09-02 13:45:53 -0400

    Allow for hex at any point in a props value

--- lib/scss_beautifier/formatters/color.rb
+++ lib/scss_beautifier/formatters/color.rb
@@ -1,4 +1,6 @@
 class SCSSBeautifier::Formatters::Color < SCSSBeautifier::Formatters::Base
+  HEX_REGEX = /(#\h{3})(?!\h)/
+
   def visit_prop(node)
     if node.value.respond_to?(:each)
       node.value.each{ |item| format_color(item) }
@@ -13,12 +15,13 @@ class SCSSBeautifier::Formatters::Color < SCSSBeautifier::Formatters::Base
         color_value = Sass::Script::Value::Color.new(color)
         color_value.options = {}
         item.instance_variable_set(:@value, Sass::Script::Value::String.new(color_value.inspect))
-      elsif matches = item.value.value.match(/([\w\W]*)(#\h{3})(?!\h)([\w\W]*)/)
-        str_start = matches[1]
-        str_end = matches[3]
-        hex = matches[2]
-        long_form = [hex[0..1], hex[1], hex[2], hex[2], hex[3], hex[3]].join
-        item.instance_variable_set(:@value, Sass::Script::Value::String.new(str_start + long_form + str_end))
+      elsif item.value.value =~ HEX_REGEX
+
+        new_val = item.value.value.split(" ").map do |val|
+          val.match(HEX_REGEX) ? [val[0..1], val[1], val[2], val[2], val[3], val[3]].join : val
+        end.join(" ")
+
+        item.instance_variable_set(:@value, Sass::Script::Value::String.new(new_val))
       end
     end
   end
